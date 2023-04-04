591e79a2b95cce5b0036fd353f2cf22c83e15b19
Asif <49162687+nhAsif@users.noreply.github.com>
2023-04-04 19:43:43 +0600

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Clone Kernel
-git clone --depth=1 https://github.com/Poco-F3-Trees/kernel_xiaomi_sm8250.git -b staging kernel
+git clone --depth=1 https://codeberg.org/LeviMarvin/kernel_xiaomi_alioth.git -b tenduo kernel
 cp -r kbuild.sh kernel/
