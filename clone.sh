6ce5cd9463b2f3206facec382128f60f649c68ab
Asif <49162687+nhAsif@users.noreply.github.com>
2023-04-04 19:56:11 +0600

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Clone Kernel
-git clone --depth=1 https://codeberg.org/LeviMarvin/kernel_xiaomi_alioth.git -b tenduo kernel
+git clone https://codeberg.org/LeviMarvin/kernel_xiaomi_alioth.git -b tenduo kernel
 cp -r kbuild.sh kernel/
