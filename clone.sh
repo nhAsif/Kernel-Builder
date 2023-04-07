1fd094d52feb14a22d5692ff26529e216ebb33b2
Asif <49162687+nhAsif@users.noreply.github.com>
2023-04-07 09:11:40 +0600

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Clone Kernel
-git clone --depth=1 https://github.com/Poco-F3-Trees/kernel_xiaomi_sm8250.git -b staging kernel
+git clone --depth=1 https://github.com/Atom-X-Devs/android_kernel_xiaomi_scarlet.git -b test kernel
 cp -r kbuild.sh kernel/
