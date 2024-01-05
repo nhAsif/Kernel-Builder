ccdc3472cf7a515fd9a814620067ed445cf6bacf
Asif <49162687+nhAsif@users.noreply.github.com>
2024-01-05 19:07:22 +0600

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Clone Kernel
-sudo apt-get install git-all
+sudo apt-get install git-all -y
 #git clone https://github.com/LOS-Sweet/kernel_xiaomi_sm8250.git -b staging kernel
 git clone https://github.com/EmanuelCN/kernel_xiaomi_sm8250.git -b staging kernel
 cp -r kbuild.sh kernel/
