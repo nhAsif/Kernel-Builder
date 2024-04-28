fb83f5a44531d14ad3ec9a88130a8b13238ec783
Asif <49162687+nhAsif@users.noreply.github.com>
2024-04-28 10:23:20 +0600

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Clone Kernel
-git clone https://github.com/sm8250-stuffs/kernel_xiaomi_sm8250-no --depth 1 kernel
+git clone https://github.com/EmanuelCN/kernel_xiaomi_sm8250 -b staging --depth 1 kernel
 cp -r kbuild.sh kernel/
