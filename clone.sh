2657929d08a4821babd67e88fb0e089d1fbc1ab4
Asif <49162687+nhAsif@users.noreply.github.com>
2024-06-24 20:57:29 +0600

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Clone Kernel
-git clone https://github.com/los-alioth/kernel_xiaomi_sm8250 --depth 1 kernel
+git clone https://github.com/UtsavBalar1231/kernel_xiaomi_sm8250 -b android14-stable --depth 1 kernel
 cp -r kbuild.sh kernel/
