cd1e34f9d5d775daf7b2f8461514642953b04bea
Asif <49162687+nhAsif@users.noreply.github.com>
2024-08-11 23:20:44 +0600

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Clone Kernel
-git clone https://github.com/crdroid-fe/kernel_xiaomi_redline -b asb_update --depth 1 kernel
+git clone https://github.com/crdroid-fe/kernel_xiaomi_redline -b test1 --depth 1 kernel
 cp -r kbuild.sh kernel/
