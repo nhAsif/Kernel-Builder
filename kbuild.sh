57d6e9d4c2a3e0d54a43a53c5c89f5daf7319c3a
Asif <49162687+nhAsif@users.noreply.github.com>
2024-08-19 21:19:51 +0600

    Update kbuild.sh

--- kbuild.sh
+++ kbuild.sh
@@ -4,7 +4,7 @@
  # Script For Building Android Kernel
 
 #ksu
-curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s v0.9.5
+curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s v0.9.4
 
 # Specify Kernel Directory
 KERNEL_DIR="$(pwd)"
