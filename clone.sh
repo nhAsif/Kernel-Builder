eb9aefceaba3640fa9598a33ca5b01a62dc57ecb
Asif <49162687+nhAsif@users.noreply.github.com>
2024-12-20 08:15:53 +0600

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Clone Kernel
-git clone https://github.com/TIMISONG-dev/kernel_xiaomi_sm8250 -b magictime-new --depth=1
+git clone https://github.com/TIMISONG-dev/kernel_xiaomi_sm8250 -b magictime-new kernel --depth=1
 cp -r kbuild.sh kernel/
